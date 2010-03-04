45c2ecb72e259e0b5375e9439f3117e6bd68d064
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-04 16:40:53 +0000

    Tweak long_desc

--- setup.py
+++ setup.py
@@ -20,9 +20,6 @@ setup(
         + '\n' +
         read("slc", "autocategorize", "README.txt")
         + '\n' +
-        'Change history\n'
-        '**************\n'
-        + '\n' +
         read("docs", "HISTORY.txt")
         ),
     # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
